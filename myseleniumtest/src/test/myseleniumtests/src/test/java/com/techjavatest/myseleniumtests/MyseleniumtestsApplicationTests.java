0f541018aca907102d6d1bdb3c70e4ee2a6bbb1f
balasr21 <bala.murugan@marksandspencer.com>
2019-11-20 16:02:08 +0000

    initialcode

---
+++ myseleniumtest/src/test/myseleniumtests/src/test/java/com/techjavatest/myseleniumtests/MyseleniumtestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techjavatest.myseleniumtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyseleniumtestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
